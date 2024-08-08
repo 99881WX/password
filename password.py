1bcf711a84b1ab82debd46a63544ecee8b636b08
unknown <99881WX.sakelu@gmail.com>
2024-08-08 17:59:17 +0800

    update ver

--- password.py
+++ password.py
@@ -2,10 +2,13 @@ t = 0
 password = 'a123456'
 while t < 3:
     pwd = input('請輸入密碼')
+    t = t + 1
     if pwd == password:
         print('密碼正確')
         break
     else:
-        pwd != password
-        print('密碼不正確，您還有',2 - t ,'次機會')
-        t = t + 1
+        print('密碼不正確')
+        if t < 3:
+            print('您還有',3 - t ,'次機會')
+        else:
+        	print('笑你被鎖帳號，再不寫起來啊lol')
